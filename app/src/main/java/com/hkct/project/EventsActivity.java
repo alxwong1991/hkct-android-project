edd5908f7892989e610699bc630910c6a01a0edf
NGYIKLOK <99123007+NGYIKLOK@users.noreply.github.com>
2022-11-20 14:00:59 +0800

    nav_drawer &  events
    modeling test

--- app/src/main/java/com/hkct/project/EventsActivity.java
+++ app/src/main/java/com/hkct/project/EventsActivity.java
@@ -3,90 +3,90 @@ package com.hkct.project;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.view.menu.ActionMenuItem;
 import androidx.drawerlayout.widget.DrawerLayout;
 
-import android.annotation.SuppressLint;
-import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
 import com.google.android.material.navigation.NavigationView;
 
-public class DiscoverActivity extends AppCompatActivity {
+public class EventsActivity extends AppCompatActivity {
 
-    private final String TAG="DiscoverActivity===>";
+    private final String TAG="EventsActivity===>";
     private TextView txtOutput;
     private DrawerLayout drawerLayout;
     private NavigationView navView;
     private ActionBarDrawerToggle actionBarDrawerToggle;
 
-    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_discover);
+        setContentView(R.layout.activity_events);
 
         txtOutput = findViewById(R.id.txtOutput);
 
+        // Navigation drawer icon always appear on the action bar
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         setNavigationDrawer();
+
+        Log.d(TAG,"AccountActivity!!!");
     }
 
-    private void setNavigationDrawer() {
+    private void setNavigationDrawer(){
+        // drawer layout instance
         drawerLayout = findViewById(R.id.drawerLayout);
-
+        // Toggle the menu icon
         actionBarDrawerToggle = new ActionBarDrawerToggle(this,drawerLayout,R.string.nav_open,R.string.nav_close);
         actionBarDrawerToggle.syncState();
 
+        // pass the toggle for the drawer layout listener
         drawerLayout.addDrawerListener(actionBarDrawerToggle);
-    }
 
+    } //setNavigationDrawer()
+
+    // override the onOptionsItemSelected() function to implement
+    // the item click listener callback to open and close the navigation
+    // drawer when the icon is clicked
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int item_id = item.getItemId();
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        Log.d(TAG,"onOptionsItemSelected->" + item.getItemId());
 
-        Log.d(TAG, "onOptionsItemSelected->" + item.getItemId());
-        if (item.getItemId() == R.id.nav_account) {
-            Log.d(TAG, "onOptionsItemSelected->" + "id=" + R.id.nav_account + "title=" + item.getTitle());
-            txtOutput.setText("Account clicked");
-        }
+//        if (item.getItemId()==R.id.nav_account){
+//            Log.d(TAG,"onOptionsItemSelected->" + "id=" + R.id.nav_account + "title=" + item.getTitle());
+//            txtOutput.setText("Account clicked");
+//        }
+
         if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
             return true;
         }
         return super.onOptionsItemSelected(item);
-    }
+    } //onOptionsItemSelected()
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.nav_menu, menu);
-        return true;
-    }
-
-    public void menu1_click(MenuItem menuItem) {
-        Log.d(TAG,"menu1_click()->" + menuItem.getItemId() + ","+ menuItem.getTitle());
+    public void menu1_click(MenuItem m){
+        Log.d(TAG,"menu1_click()->" + m.getItemId() + ","+ m.getTitle());
         txtOutput.setText(R.string.msg1);
         txtOutput.setTextColor(Color.RED);
+
+
         drawerLayout.closeDrawers();
     }
-
-    public void menu2_click(MenuItem menuItem) {
-        Log.d(TAG,"menu2_click()->" + menuItem.getItemId() + ","+ menuItem.getTitle());
+    public void menu2_click(MenuItem m){
+        Log.d(TAG,"menu2_click()->" + m.getItemId() + ","+ m.getTitle());
         txtOutput.setText(R.string.msg2);
-        txtOutput.setTextColor(Color.RED);
+        txtOutput.setTextColor(Color.BLUE);
+
         drawerLayout.closeDrawers();
     }
-
-    public void menu3_click(MenuItem menuItem) {
-        Log.d(TAG,"menu3_click()->" + menuItem.getItemId() + ","+ menuItem.getTitle());
+    public void menu3_click(MenuItem m){
+        Log.d(TAG,"menu3_click()->" + m.getItemId() + ","+ m.getTitle());
         txtOutput.setText(R.string.msg3);
-        txtOutput.setTextColor(Color.RED);
+        txtOutput.setTextColor(Color.GREEN);
         drawerLayout.closeDrawers();
     }
 
@@ -97,12 +97,25 @@ public class DiscoverActivity extends AppCompatActivity {
         drawerLayout.closeDrawers();
     }
 
-
-    public void menu4_click(MenuItem menuItem) {
-        Log.d(TAG,"menu4_click()->" + menuItem.getItemId() + ","+ menuItem.getTitle());
-        txtOutput.setTextColor(Color.RED);
+    public void menu4_click(MenuItem m){
+        Log.d(TAG,"menu3_click()->" + m.getItemId() + ","+ m.getTitle());
+        txtOutput.setText(R.string.msg4);
+        txtOutput.setTextColor(Color.CYAN);
         startActivity(new Intent(this, LoginActivity.class));
         overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
         drawerLayout.closeDrawers();
     }
+
+    public void backClick(View v){
+        startActivity(new Intent(this,AddEventsActivity.class));
+
+        int version = Integer.valueOf(android.os.Build.VERSION.SDK);
+        if(version >=5){
+
+//            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+            overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+        }
+        this.finish();
+    }
+
 }
