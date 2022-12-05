e7cabad6707e6ffe8c966137827715ab4bcfd26a
NGYIKLOK <99123007+NGYIKLOK@users.noreply.github.com>
2022-12-05 17:05:10 +0800

    stripe payment
    function  done
    layout will coming soon

--- app/src/main/java/com/hkct/project/MembershipActivity.java
+++ app/src/main/java/com/hkct/project/MembershipActivity.java
@@ -1,6 +1,7 @@
 package com.hkct.project;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
@@ -9,15 +10,47 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.material.navigation.NavigationView;
+import com.stripe.android.PaymentConfiguration;
+import com.stripe.android.paymentsheet.PaymentSheet;
+import com.stripe.android.paymentsheet.PaymentSheetResult;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import retrofit2.http.POST;
 
 public class MembershipActivity extends AppCompatActivity {
 
     private final String TAG="MembershipActivity===>";
     private DrawerLayout drawerLayout;
     private ActionBarDrawerToggle actionBarDrawerToggle;
+
+    //Stripe 1
+    Button button;
+    String SECRET_KEY = "sk_test_51MBVL8FlCzf4JXifpD5y1j0SW5rk5d5UwFWbcCiUWc3hg5x6mxj8BrNaH05bmOKkO6iCLH7fh6AIJ8QNZyRn7Wjh00fSefZas4";
+    String PUBLISH_KEY = "pk_test_51MBVL8FlCzf4JXifoVqhKPCcwCNvM32gwfs3p34f0c4ZzSG4MROMPDBfIQdQReaq6bIbBi0ZlQJt4s0d25jqlSE100CYMwm02F";
+    PaymentSheet paymentSheet;
+
+    String customerID;
+    String EphericalKey;
+    String ClientSecret;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,7 +67,179 @@ public class MembershipActivity extends AppCompatActivity {
 
         Log.d(TAG,"===>MembershipActivity!!!");
 
+        //Stripe 2
+        button = findViewById(R.id.btn);
+
+        PaymentConfiguration.init(this, PUBLISH_KEY);
+
+        paymentSheet = new PaymentSheet(this, paymentSheetResult -> {
+
+            onPaymentResult(paymentSheetResult);
+        });
+
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PaymentFlow();
+            }
+        });
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST,
+                "https://api.stripe.com/v1/customers",
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                        try {
+                            JSONObject object = new JSONObject(response);
+                            customerID = object.getString("id");
+                            Toast.makeText(MembershipActivity.this, customerID, Toast.LENGTH_SHORT).show();
+
+
+                            getEphericalKey(customerID);
+
+                        }catch (JSONException e){
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        }){
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> header = new HashMap<>();
+                //             "Bearer "  -- Must be spacebar in Bearer at the  end
+                header.put("Authorization", "Bearer " + SECRET_KEY);
+                return header;
+            }
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(MembershipActivity.this);
+        requestQueue.add(stringRequest);
+
     }//onCreate
+
+    private void onPaymentResult(PaymentSheetResult paymentSheetResult) {
+        if(paymentSheetResult instanceof PaymentSheetResult.Completed){
+            Toast.makeText(this, "payment success", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    private void getEphericalKey(String customerID) {
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST,
+                "https://api.stripe.com/v1/ephemeral_keys",
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                        try {
+                            JSONObject object = new JSONObject(response);
+                            EphericalKey = object.getString("id");
+                            Toast.makeText(MembershipActivity.this, EphericalKey, Toast.LENGTH_SHORT).show();
+
+                            getClientSecret(customerID, EphericalKey);
+
+                        }catch (JSONException e){
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        }){
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> header = new HashMap<>();
+//             "Bearer "  -- Must be spacebar in Bearer at the  end
+                header.put("Authorization", "Bearer " + SECRET_KEY);
+                header.put("Stripe-Version", "2022-11-15");
+                return header;
+            }
+
+            @Nullable
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("customer", customerID);
+                return params;
+            }
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(MembershipActivity.this);
+        requestQueue.add(stringRequest);
+    }
+
+    private void getClientSecret(String customerID, String ephericalKey) {
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST,
+                "https://api.stripe.com/v1/payment_intents",
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                        try {
+                            JSONObject object = new JSONObject(response);
+                            ClientSecret = object.getString("client_secret");
+                            Toast.makeText(MembershipActivity.this, ClientSecret, Toast.LENGTH_SHORT).show();
+
+
+
+                        }catch (JSONException e){
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        }){
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> header = new HashMap<>();
+                //             "Bearer "  -- Must be spacebar in Bearer at the  end
+                header.put("Authorization", "Bearer " + SECRET_KEY);
+                return header;
+            }
+
+            @Nullable
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("customer", customerID);
+                params.put("amount", "999"+"00");
+                params.put("currency", "usd");
+                params.put("automatic_payment_methods[enabled]", "true");
+                return params;
+            }
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(MembershipActivity.this);
+        requestQueue.add(stringRequest);
+
+    }
+
+    private void PaymentFlow() {
+
+        paymentSheet.presentWithPaymentIntent(
+                ClientSecret, new PaymentSheet.Configuration("ABC Company"
+                , new PaymentSheet.CustomerConfiguration(
+                        customerID,
+                        EphericalKey
+                ))
+        );
+
+
+    }
 
     private void setNavigationDrawer(){
         // drawer layout instance
