1ee10858d64beb618074c46db9e4470546115b4a
Wong Sau Hang Alex <alxwong1991@gmail.com>
2022-12-06 02:41:06 +0800

    done comments function

--- app/src/main/java/com/hkct/project/CommentsActivity.java
+++ app/src/main/java/com/hkct/project/CommentsActivity.java
@@ -1,6 +1,7 @@
 package com.hkct.project;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -14,11 +15,21 @@ import android.widget.Toast;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentChange;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.hkct.project.Adapter.CommentsAdapter;
+import com.hkct.project.Model.Comments;
+import com.hkct.project.Model.Users;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class CommentsActivity extends AppCompatActivity {
@@ -30,6 +41,9 @@ public class CommentsActivity extends AppCompatActivity {
     private String post_id;
     private String currentUserId;
     private FirebaseAuth auth;
+    private CommentsAdapter adapter;
+    private List<Comments> mList;
+    private List<Users> usersList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,10 +58,42 @@ public class CommentsActivity extends AppCompatActivity {
         auth = FirebaseAuth.getInstance();
         currentUserId = auth.getCurrentUser().getUid();
 
-        post_id = getIntent().getStringExtra("postid");
+        mList = new ArrayList<>();
+        usersList = new ArrayList<>();
+        adapter = new CommentsAdapter(CommentsActivity.this, mList, usersList);
 
+        post_id = getIntent().getStringExtra("postid");
         mCommentRecyclerView.setHasFixedSize(true);
         mCommentRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mCommentRecyclerView.setAdapter(adapter);
+
+        firestore.collection("Posts/" + post_id + "/Comments").addSnapshotListener(CommentsActivity.this, new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+                for (DocumentChange documentChange : value.getDocumentChanges()) {
+                    if (documentChange.getType() == DocumentChange.Type.ADDED) {
+                        Comments comments = documentChange.getDocument().toObject(Comments.class);
+                        String userId = documentChange.getDocument().getString("user");
+
+                        firestore.collection("Users").document(userId).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                                if (task.isSuccessful()) {
+                                    Users users = task.getResult().toObject(Users.class);
+                                    usersList.add(users);
+                                    mList.add(comments);
+                                    adapter.notifyDataSetChanged();
+                                } else {
+                                    Toast.makeText(CommentsActivity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+                    } else {
+                        adapter.notifyDataSetChanged();
+                    }
+                }
+            }
+        });
 
         mAddCommentBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -58,7 +104,7 @@ public class CommentsActivity extends AppCompatActivity {
                     commentsMap.put("comment", comment);
                     commentsMap.put("time", FieldValue.serverTimestamp());
                     commentsMap.put("user", currentUserId);
-                    firestore.collection("Posts" + post_id + "/Comments").add(commentsMap).addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
+                    firestore.collection("Posts/" + post_id + "/Comments").add(commentsMap).addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
                         @Override
                         public void onComplete(@NonNull Task<DocumentReference> task) {
                             if (task.isSuccessful()) {
