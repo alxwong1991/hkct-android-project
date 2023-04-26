320031db412989d305daa1bd68a8a69444881e0a
Wong Sau Hang Alex <40209618+alxwong1991@users.noreply.github.com>
2023-04-26 11:19:47 +0800

    fix for each product layout

--- app/src/main/java/com/hkct/project/Adapter/ChatAdapter.java
+++ app/src/main/java/com/hkct/project/Adapter/ChatAdapter.java
@@ -1,40 +1,40 @@
-package com.hkct.project.Adapter;
-
-import android.app.Activity;
-import android.view.ViewGroup;
-import androidx.annotation.NonNull;
-import androidx.recyclerview.widget.RecyclerView;
-import com.hkct.project.Model.Chat;
-import com.hkct.project.Model.Users;
-import java.util.List;
-
-public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.ChatViewHolder> {
-
-    private Activity context;
-    private List<Chat> chatList;
-    private List<Users> usersList;
-
-    public ChatAdapter(Activity context, List<Chat> chatList, List<Users> usersList) {
-        this.context = context;
-        this.usersList = usersList;
-        this.chatList = chatList;
-    }
-
-    @NonNull
-    @Override
-    public ChatAdapter.ChatViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
-    }
-
-    @Override
-    public void onBindViewHolder(@NonNull ChatAdapter.ChatViewHolder holder, int position) {
-
-    }
-
-    @Override
-    public int getItemCount() {
-        return 0;
-    }
-}
+//package com.hkct.project.Adapter;
+//
+//import android.app.Activity;
+//import android.view.ViewGroup;
+//import androidx.annotation.NonNull;
+//import androidx.recyclerview.widget.RecyclerView;
+//import com.hkct.project.Model.Chat;
+//import com.hkct.project.Model.Users;
+//import java.util.List;
+//
+//public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.ChatViewHolder> {
+//
+//    private Activity context;
+//    private List<Chat> chatList;
+//    private List<Users> usersList;
+//
+//    public ChatAdapter(Activity context, List<Chat> chatList, List<Users> usersList) {
+//        this.context = context;
+//        this.usersList = usersList;
+//        this.chatList = chatList;
+//    }
+//
+//    @NonNull
+//    @Override
+//    public ChatAdapter.ChatViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+//        return null;
+//    }
+//
+//    @Override
+//    public void onBindViewHolder(@NonNull ChatAdapter.ChatViewHolder holder, int position) {
+//
+//    }
+//
+//    @Override
+//    public int getItemCount() {
+//        return 0;
+//    }
+//}
 
 
